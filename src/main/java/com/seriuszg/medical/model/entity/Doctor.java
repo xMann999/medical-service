4016230dc453dade2f1f140d62155e107064d44d
Sergiusz Gołacki <sergoisz.golacki@gmail.com>
2023-07-01 17:29:35 +0200

    added doctor class

---
+++ src/main/java/com/seriuszg/medical/model/entity/Doctor.java
@@ -0,0 +1,27 @@
+package com.seriuszg.medical.model.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String specialisation;
+
+}
