225a77dcc78f3ce777514f5766f7713f10a8b127
Sergiusz Gołacki <sergoisz.golacki@gmail.com>
2023-07-06 12:44:05 +0200

    fixed

--- src/main/java/com/seriuszg/medical/exceptions/IncorrectEmailException.java
+++ src/main/java/com/seriuszg/medical/exceptions/IncorrectEmailException.java
@@ -1,15 +1,14 @@
 package com.seriuszg.medical.exceptions;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
 @Getter
 @Setter
-public class PatientIllegalDataException extends PatientExceptions {
+public class IncorrectEmailException extends PatientExceptions {
 
-    public PatientIllegalDataException() {
+    public IncorrectEmailException() {
         super("Wpisz poprawny adres e-mail", HttpStatus.NOT_FOUND);
     }
 }
