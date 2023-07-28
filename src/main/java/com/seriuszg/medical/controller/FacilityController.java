e9562baaaf6f3d0b8e190dc1faf6f545f9ba511b
Sergiusz Gołacki <sergoisz.golacki@gmail.com>
2023-07-29 01:21:44 +0200

    added documentation

--- src/main/java/com/seriuszg/medical/controller/FacilityController.java
+++ src/main/java/com/seriuszg/medical/controller/FacilityController.java
@@ -1,9 +1,17 @@
 package com.seriuszg.medical.controller;
 
+import com.seriuszg.medical.model.dto.DoctorDto;
 import com.seriuszg.medical.model.dto.FacilityDto;
 import com.seriuszg.medical.model.dto.FacilityRegistrationDto;
 import com.seriuszg.medical.service.FacilityService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,16 +23,33 @@ public class FacilityController {
 
     private final FacilityService facilityService;
 
+    @Operation(summary = "Create a new facility", tags = "Facility")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = FacilityDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid request data", content = @Content)
+    })
     @PostMapping
     public FacilityDto saveFacility(@RequestBody FacilityRegistrationDto facilityRegistrationDto) {
         return facilityService.saveFacility(facilityRegistrationDto);
     }
 
+    @Operation(summary = "Delete facility", tags = "Facility")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = FacilityDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Facility not found", content = @Content)
+    })
     @DeleteMapping("/{id}")
     public FacilityDto deleteFacility (@PathVariable Long id) {
         return facilityService.deleteFacility(id);
     }
 
+    @Operation(summary = "Get information about all existing facilities", tags = "Facility")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, array = @ArraySchema(schema = @Schema (implementation = FacilityDto.class)))})
+    })
     @GetMapping
     public List<FacilityDto> showAllFacilities() {
         return facilityService.getAllFacilities();
--- src/main/java/com/seriuszg/medical/controller/VisitController.java
+++ src/main/java/com/seriuszg/medical/controller/VisitController.java
@@ -1,9 +1,16 @@
 package com.seriuszg.medical.controller;
 
+import com.seriuszg.medical.model.dto.PatientDto;
 import com.seriuszg.medical.model.dto.VisitRequest;
 import com.seriuszg.medical.model.dto.VisitResponse;
 import com.seriuszg.medical.service.VisitService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,11 +20,23 @@ public class VisitController {
 
     private final VisitService visitService;
 
+    @Operation(summary = "Create a new visit", tags = "Visit")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = VisitResponse.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid request data", content = @Content)
+    })
     @PostMapping
     public VisitResponse createVisit(@RequestBody VisitRequest visitRequest) {
         return visitService.requestVisit(visitRequest);
     }
 
+    @Operation(summary = "Assign patient to visit", tags = "Visit")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = VisitResponse.class))}),
+            @ApiResponse(responseCode = "400", description = "Patient or visit not found", content = @Content)
+    })
     @PatchMapping("/{visitId}/assignPatient")
     public VisitResponse assignPatientToVisit(@RequestBody Long patientId, @PathVariable Long visitId) {
         return visitService.assignPatient(visitId, patientId);
